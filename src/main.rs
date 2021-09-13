4868cce6eb7e9a43026ff114299ccf8dfffc4325
i-zanis <ioanlafazanis@gmail.com>
2021-09-13 22:44:19 +0100

    refactor(main): add parse_config()

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,23 @@ use std::fs;
 
 fn main() {
   let args: Vec<String> = env::args().collect();
+  let (query, file_name) = parse_config(&args);
 
-  let query = &args[1];
-  let file_name = &args[2];
+  print!("Seeking {}", config.query);
+  print!("In the file {}", config.file_name);
 
-  println!("Seeking {}", query);
-  println!("Found in {}", file_name);
+  let contents = fs::read_to_string(config.file_name)
+    .expect("There was an unknown error");
+}
 
-  let contents = fs::read_to_string(file_name)
-    .expect("Unexpected error when reading the file.");
+struct Config {
+  query: String,
+  file_name: String,
+}
 
-  println!("With text:\n{}", contents);
+fn parse_config(args: &[String]) -> Config {
+  let query = args[1].clone();
+  let file_name = args[2].clone();
+
+  Config { query, file_name }
 }
