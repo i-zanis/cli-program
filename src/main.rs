b7c2c39780979af9e6303f72c5ed071f02c724e1
i-zanis <ioanlafazanis@gmail.com>
2021-09-13 13:05:49 +0100

    feat(main): update body|search file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+  let args: Vec<String> = env::args().collect();
+
+  let query = &args[1];
+  let file_name = &args[2];
+
+  println!("Seeking {}", query);
+  println!("Found in {}", file_name);
+
+  let contents = fs::read_to_string(file_name)
+    .expect("Unexpected error when reading the file.");
+
+  println!("With text:\n{}", contents);
 }
