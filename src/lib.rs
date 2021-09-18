d4d28318da569af6fa1071949b87fb8e8278af7b
i-zanis <ioanlafazanis@gmail.com>
2021-09-18 11:47:29 +0100

    refactor(lib): add Config struct

--- src/lib.rs
+++ src/lib.rs
@@ -32,6 +32,11 @@ Ke$ha";
   }
 }
 
+pub struct Config {
+  pub query: String,
+  pub file_name: String,
+  pub case_sensitive: bool,
+}
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
   for line in contents.lines() {
     if line.contains(query) {
@@ -49,3 +54,19 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
   }
   Ok(())
 }
+
+pub fn search_case_insensitive<'a> (
+  query: &str,
+  contents: &'a str,
+)-> Vec<&'a str> {
+  let query = query.to_lowercase();
+  let mut results = Vec::new();
+
+  for line in contents.lines() {
+    if line.to_lowercase().contains(&query) {
+      results.push(line);
+    }
+  }
+  results
+}
+
