1538aa1b41bd67fba59c11692af7fbec188ff7e2
i-zanis <ioanlafazanis@gmail.com>
2021-09-27 07:19:12 +0100

    refactor(main): remove redundant code that was migrated to lib

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,20 @@
 use std::env;
-use std::fs;
+use std::process;
+
+use minigrep::Config;
+use cli_program::Config;
 
 fn main() {
   let args: Vec<String> = env::args().collect();
-  let (query, file_name) = parse_config(&args);
 
-  print!("Seeking {}", config.query);
-  print!("In the file {}", config.file_name);
+  let config = Config::new(&args).unwrap_or_else(|err| {
+    eprintln!("Parsing arguments error: {}", err);
+    process::exit(1);
+  });
 
-  if let Err(e) = run(config) {
-    print!("Application error: {}", e);
+  if let Err(e) = minigrep::run(config) {
+    eprintln!("Error: {}", e);
+
     process::exit(1);
   }
 }
-
-struct Config {
-  query: String,
-  file_name: String,
-}
-
-fn parse_config(args: &[String]) -> Config {
-  let query = args[1].clone();
-  let file_name = args[2].clone();
-
-  Config { query, file_name }
-}
-
-fn new(args: &[String]) -> Result<Config, &str> {
-  if args.len() < 3 {
-    return Err("Insufficient arguments");
-  }
-  let query = args[1].clone();
-  let file_name = args[2].clone();
-
-  Ok(Config { query, file_name })
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-  let contents = fs::read_to_string(config.file_name)?;
-
-  println!("With text:\n{}", contents);
-
-  Ok(()) // call run for side effects only
-}
-
-
