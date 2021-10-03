5ad77b2b01212a86b57b16c2cbd3b7d548b4e754
i-zanis <ioanlafazanis@gmail.com>
2021-10-03 01:24:51 +0100

    refactor(main): change method signature|naming convention on project

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,15 @@
 use std::env;
 use std::process;
-
-use minigrep::Config;
 use cli_program::Config;
 
 fn main() {
-  let args: Vec<String> = env::args().collect();
-
-  let config = Config::new(&args).unwrap_or_else(|err| {
+  let config = Config::new(env::args()).unwrap_or_else(|err| {
     eprintln!("Parsing arguments error: {}", err);
     process::exit(1);
   });
 
-  if let Err(e) = minigrep::run(config) {
-    eprintln!("Error: {}", e);
-
+  if let Err(e) = cli_program::run(config) {
+    eprintln!("Application error: {}", e);
     process::exit(1);
   }
 }
