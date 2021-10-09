ed68f6c3cbaa7ed9f89d6c6fc2e843eadaf4e3cc
i-zanis <ioanlafazanis@gmail.com>
2021-10-09 20:09:39 +0100

    fix(lib): typo print -> println!

--- src/lib.rs
+++ src/lib.rs
@@ -73,7 +73,7 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     search_case_insensitive(&config.query, &contents)
   };
   for line in results {
-    print("{}", line);
+    println!("{}", line);
   }
   Ok(())
 }
